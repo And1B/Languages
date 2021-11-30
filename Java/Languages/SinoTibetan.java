1ce6aabce252c4589d9a0485d615199bd19cebca
Andi <andi.buhn@gmail.com>
2021-11-30 19:54:02 +0100

    exporting project into own folder

---
+++ Java/Languages/SinoTibetan.java
@@ -0,0 +1,10 @@
+package Languages;
+public class SinoTibetan extends Language {
+
+    SinoTibetan(String langName, int speakers){
+        super(langName, speakers, "Asia", "subject-object-verb");
+        if(langName.contains("Chinese")){
+            wordOrder = "subject-verb-object";
+        }
+    }
+}
